5a976b9cfeb5eace25773d49fd0370c03994c84d
ChrisMeyer7088 <chrismeyer175@gmail.com>
2018-12-28 11:43:57 -0800

    Setup Registration Page

---
+++ FrontEnd/Angular-FillMyTime/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RegistrationPageComponent } from './registration-page/registration-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RegistrationPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
